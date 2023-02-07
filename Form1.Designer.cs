415d475073e3602b6e2b7c40f3e627330eb92c9f
DuradazhiVlad <dvlad1995p@gmail.com>
2023-02-07 18:12:59 +0200

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace WinFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(31, 23);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            2000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(360, 55);
+            this.numericUpDown1.TabIndex = 0;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1353, 652);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(225, 69);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Location = new System.Drawing.Point(1670, 80);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(600, 300);
+            this.panel1.TabIndex = 3;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel2.Location = new System.Drawing.Point(12, 263);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1620, 100);
+            this.panel2.TabIndex = 4;
+            this.panel2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel2_MouseClick);
+            this.panel2.MouseHover += new System.EventHandler(this.panel2_MouseHover);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(20F, 48F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1644, 750);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.MouseEnter += new System.EventHandler(this.Form1_MouseEnter);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private NumericUpDown numericUpDown1;
+        private Button button1;
+        private Panel panel1;
+        private Panel panel2;
+        private Panel panel3;
+        private NumericUpDown numericUpDown2;
+        private Button button2;
+        private Button button3;
+        private Panel panel4;
+        private NumericUpDown numericUpDown3;
+        private Panel panel5;
+        private NumericUpDown numericUpDown4;
+        private Button button4;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.VisualBasic.Devices;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Windows.Forms.LinkLabel;
+using System.Windows.Input;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
+using System.Windows.Forms;
+using System.Reflection.Emit;
+using Application = System.Windows.Forms.Application;
+
+namespace WinFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+
+        public void Form1_Load_1(object sender, EventArgs e)
+        {
+
+            MessageBox.Show("Резюме   Мене звати Влад");
+            string FormSize = this.Size.ToString();
+            
+        }
+        //Коорлинати мишки
+        private void Form1_MouseEnter(object sender, EventArgs e)
+        {
+            if (true)
+            {
+            this.Text = MousePosition.ToString();
+
+            }
+
+
+            
+
+        }
+
+
+
+        
+        //При натискані мишки
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            
+            if (e.Button == MouseButtons.Left)
+            {
+
+            MessageBox.Show("Мишка знаходиться зовні");
+            }
+            if (e.Button == MouseButtons.Right)
+            {
+
+                MessageBox.Show(SizeFromClientSize(Size).ToString());
+            }
+
+            if (e.Button == MouseButtons.Left&& Control.ModifierKeys == Keys.Shift)
+            {
+            Form1 form=new Form1();
+            form.Close();
+                
+            }
+
+            
+        }
+        private void panel_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Control)
+            {
+                Form1 form = new Form1();
+                form.Close();
+            }
+        }
+
+
+
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            decimal s = numericUpDown1.Value;
+            var rand = new Random();
+            for (int i = 0; i < 1999; i++)
+            {
+                int ra = rand.Next(1, 2000);
+
+                if (ra == numericUpDown1.Value)
+                {
+                    MessageBox.Show("Користувач загадав число " + ra.ToString());
+                    MessageBox.Show("Програмі знадобилося спроб " + i);
+
+                }
+            }
+            MessageBox.Show("Розмір програми " + SizeFromClientSize(ClientSize));
+            MessageBox.Show("Координати мишки ");
+
+        }
+
+        
+
+        
+
+        private void panel2_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+
+                MessageBox.Show("Мишка знаходиться в середині");
+                
+            }
+            if (e.Button == MouseButtons.Left && Keys.KeyCode == Keys.Control)
+            {
+                this.Close();
+            }
+
+
+        }
+
+        private void panel2_MouseHover(object sender, EventArgs e)
+        {
+            //if (e. == MouseButtons.Left)
+            //{
+
+            //    MessageBox.Show("fdg");
+            //}
+        }
+
+        
+    }
+    
+}
